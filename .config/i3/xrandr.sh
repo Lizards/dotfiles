508234759e21c0a1485753f1459be88a8025d5ff
Lizards <amanda@smart.net>
2018-03-25 16:24:18 -0400

    xrandr startup script

---
+++ .config/i3/xrandr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-1 --off --output DP-3 --mode 3840x2160 --pos 3840x0 --rotate normal --output DVI-D-1 --off --output DP-1 --off --output DP-2 --mode 3840x2160 --pos 0x0 --rotate normal --dpi 220
